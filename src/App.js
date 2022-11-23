b837f8f458d9f635bfe91ffb907e50aca80b214c
SHANGEETH <shanshangeeth@gmail.com>
2022-11-23 16:55:47 +0530

    nn

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoutes>
+              <Login />
+            </PublicRoutes>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoutes>
+              <Register />
+            </PublicRoutes>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export function ProtectedRoutes({ children }) {
+  const user = localStorage.getItem("user");
+  if (user !== "" && user) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+}
+
+export function PublicRoutes({ children }) {
+  const user = localStorage.getItem("user");
+  if (user !== "" && user) {
+    return <Navigate to="/" />;
+  } else {
+    return children;
+  }
+}
+
+export default App;
---
+++ src/pages/Register.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigate = useNavigate();
+  const registerUser = async () => {
+    if (name.length < 2) {
+      toast.error("name must be minium 2 characters");
+    } else {
+      if (password === confirmPassword && confirmPassword.length > 5) {
+        const userObj = {
+          name,
+          password,
+          email,
+          confirmPassword,
+        };
+        try {
+          toast.loading("Loading...");
+          const response = await axios.post(
+            "http://localhost:3110/api/auth/register",
+            userObj
+          );
+          toast.dismiss();
+          if (response.data.success) {
+            toast.success(response.data.message);
+            navigate("/login");
+          } else {
+            toast.error(response.data.message);
+          }
+        } catch (error) {
+          toast.dismiss();
+          toast.error("Something went wrong");
+        }
+      } else {
+        toast.error("minimum 6 characters / Passwords Not Matched");
+      }
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen space-x-40">
+      <div className="w-[400px] flex space-y-5 flex-col shadow-lg border border-gray-300">
+        <h1 className="font-semibold text-2xl text-white bg-primary p-5 rounded-b-full text-center">
+          Nice To meet you
+        </h1>
+
+        <div className="flex flex-col space-y-5 p-5">
+          <input
+            type="text"
+            className="py-1 px-3 border-2 border-secondary focus:outline-none w-full"
+            placeholder="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <input
+            type="text"
+            className="py-1 px-3 border-2 border-secondary focus:outline-none w-full"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <input
+            type="password"
+            className="py-1 px-3 border-2 border-secondary focus:outline-none w-full"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <input
+            type="password"
+            className="py-1 px-3 border-2 border-secondary focus:outline-none w-full"
+            placeholder="confirm password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={confirmPassword}
+          />
+
+          <div className="flex justify-between items-end">
+            <Link className="underline text-primary" to="/login">
+              Click Here To Login
+            </Link>
+
+            <button
+              className="py-1 px-5 text-white bg-primary"
+              onClick={registerUser}
+            >
+              REGISTER
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
